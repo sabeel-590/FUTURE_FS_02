a17b50490f9ea1f927bbb5195b144c5762a16551
sabeel-590 <mdsabeelkhan08@gmail.com>
2026-07-11 00:06:47 +0530

    Configured GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/FUTURE_FS_02/',
   plugins: [react()],
   server: {
     port: 5173,
